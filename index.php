6b8e5be9fa84b7021707015cc67c267be052ef9d
Claudia Lim <climweiching@gmail.com>
2023-09-25 21:13:07 +0100

    basic html

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Paint Collection App</title>
+
+    <meta name="description" content="Paint Collection App">
+    <meta name="author" content="Claudia Lim">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+          integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+          crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="icon" href="images/favicon.png" sizes="192x192">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon.png">
+
+    <!-- <script defer src="js/index.js"></script> -->
+
+</head>
+
+<body>
+<div class="grid">
+    <header>
+        <h1>Paint Collection App</h1>
+    </header>
+
+    <nav>
+        <a href=""><i class="fa-solid fa-plus"></i>Add New Paint</a>
+    </nav>
+
+    <section class="paint-container">
+        <div class="image-container">
+            <img alt='placeholder image' src="https://placedog.net/150/150">
+        </div>
+        <div class="paint-info">
+            <table>
+                <tr>
+                    <td>Brand:</td>
+                    <td>brand name</td>
+                </tr>
+                <tr>
+                    <td>Colour:</td>
+                    <td>colour</td>
+                </tr>
+                <tr>
+                    <td>Needs replacing?</td>
+                    <td>yes/no</td>
+                </tr>
+            </table>
+        </div>
+    </section>
+
+
+    <footer>
+        Claudia Lim 2023
+    </footer>
+</div>
+</body>
+</html>
